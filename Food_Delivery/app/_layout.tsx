a7787a9fce42347e8343b296218552a9b8a41bf0
unawarexi <unawarexi@gmail.com>
2024-11-09 05:45:56 +0100

    Expo | setting up and customization

--- Food_Delivery/app/_layout.tsx
+++ Food_Delivery/app/_layout.tsx
@@ -1,4 +1,4 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
@@ -11,7 +11,7 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
@@ -27,11 +27,10 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+        
       </Stack>
-    </ThemeProvider>
+   
   );
 }
---
+++ Food_Delivery/app/splashScreen.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const splashScreen = () => {
+  return (
+    <View>
+      <Text>splashScreen</Text>
+    </View>
+  )
+}
+
+export default splashScreen
+
+const styles = StyleSheet.create({
+  
+})
